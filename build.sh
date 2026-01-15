c6a2d944bd0195158d77ddcae244c06fa9104d5a
Campus OCR <admin@campusocr.local>
2026-01-15 15:13:39 +0530

    Add build script for Tesseract installation

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install system dependencies
+apt-get update
+apt-get install -y tesseract-ocr tesseract-ocr-eng
+
+# Install Python dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Django setup
+python manage.py collectstatic --no-input
+python manage.py migrate
